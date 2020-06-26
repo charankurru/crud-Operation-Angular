8c7dc5f0caf238ca36148bb02de78a3a7d5f1dfd
charankurru <61787496+charankurru@users.noreply.github.com>
2020-06-26 12:28:38 +0530

    path added:

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var path = require('path');
 const cors = require('cors');
 
 const { mongoose } = require('./db.js');
